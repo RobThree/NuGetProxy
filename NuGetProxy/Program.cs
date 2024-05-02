8d60432c0feb88fce768a95842b23bc14362971d
RobIII <rob@devcorner.nl>
2024-05-02 22:50:44 +0200

    Add project files.

---
+++ NuGetProxy/Program.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Text;
+using Yarp.ReverseProxy.Configuration;
+using Yarp.ReverseProxy.Transforms;
+using Yarp.ReverseProxy.Transforms.Builder;
+
+namespace ConsoleApp5;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var webappbuilder = WebApplication.CreateBuilder(args);
+        webappbuilder.Configuration.AddUserSecrets<Program>();
+
+        var config = webappbuilder.Configuration;
+        var pathbase = config.GetValue<string>("PathBase") ?? string.Empty;
+
+        webappbuilder
+            .Services
+            .AddReverseProxy()
+            .ConfigureHttpClient((context, handler) =>
+            {
+                handler.AutomaticDecompression = DecompressionMethods.All;
+            })
+            .LoadFromConfig(config.GetSection("ReverseProxy"))
+            .AddTransforms(transformContext =>
+            {
+                transformContext.AddResponseTransform(async responseContext =>
+                {
+                    // Only handle json responses and only if we have destinations
+                    if (IsResponseOfType(responseContext.ProxyResponse, "application/json")
+                        && TryGetDestinationConfig(transformContext, "NuGet", out var destconfig))
+                    {
+                        // Read response body
+                        using var reader = new StreamReader(await responseContext.ProxyResponse!.Content.ReadAsStreamAsync(responseContext.CancellationToken));
+                        var body = await reader.ReadToEndAsync(responseContext.CancellationToken);
+
+                        if (!string.IsNullOrEmpty(body))
+                        {
+                            responseContext.SuppressResponseBody = true;
+
+                            // Rewrite urls in response body
+                            var baseuri = GetBaseUri(responseContext.HttpContext.Request, pathbase);
+                            body = body.Replace(destconfig.Address, baseuri, StringComparison.OrdinalIgnoreCase);
+
+                            // Set new response length
+                            responseContext.HttpContext.Response.Headers.ContentLength = body.Length;
+                            // Output rewritten response body
+                            await responseContext.HttpContext.Response.Body.WriteAsync(Encoding.UTF8.GetBytes(body), responseContext.CancellationToken);
+                        }
+                    }
+                });
+            });
+
+        var app = webappbuilder.Build();
+        app.MapReverseProxy();
+        app.UsePathBase(pathbase);
+
+        await app.RunAsync();
+    }
+
+    public static string GetBaseUri(HttpRequest request, string path)
+        => new UriBuilder
+        {
+            Scheme = request.Scheme,
+            Host = request.Host.Host,
+            Port = request.Host.Port ?? (request.IsHttps ? 443 : 80),
+            Path = path
+        }.Uri.AbsoluteUri.TrimEnd('/');
+
+    private static bool IsResponseOfType(HttpResponseMessage? response, string contentType)
+        => contentType.Equals(response?.Content?.Headers?.ContentType?.MediaType, StringComparison.OrdinalIgnoreCase);
+
+    public static bool TryGetDestinationConfig(TransformBuilderContext context, string key, [NotNullWhen(true)] out DestinationConfig? destinationConfig)
+    {
+        destinationConfig = default;
+        return context.Cluster?.Destinations is not null && context.Cluster.Destinations.TryGetValue(key, out destinationConfig);
+    }
+}
---
+++ README.md
@@ -0,0 +1,83 @@
+# NuGet proxy
+
+This is a simple proxy for NuGet servers that insist on requiring authentication (looking at you, GitLab) but where you want to allow public access to the packages. This project uses [YARP](https://microsoft.github.io/reverse-proxy/index.html) to (reverse) proxy requests to the NuGet server and adds an accesstoken to each request. Any responses to `*.json` url's that return an `application/json` response will then be rewritten to contain the URL of the proxy instead of the actual NuGet server behind the proxy so everything keeps working as expected.
+
+## Usage
+
+To configure the proxy, change the appsettings:
+
+```json
+{
+  "Kestrel": {
+    "Endpoints": {
+      "Http": {
+        "Url": "http://my.nuget.org:5000"
+      },
+      //"Https": {
+      //  "Url": "https://my.nuget.org:5001",
+      //  "Certificate": {
+      //    "Path": "my.nuget.org.pfx",
+      //    "Password": "ExAmplePa$$w0rd*&"
+      //  }
+      //}
+    }
+  },
+  "PathBase": "/NuGet",
+  "ReverseProxy": {
+    "Routes": {
+      "CatchAllRoute": {
+        "ClusterId": "NuGetCluster",
+        "Match": {
+          "Path": "{**catch-all}"
+        },
+        "Transforms": [
+          {
+            "RequestHeader": "X-NuGet-ApiKey",
+            "Set": "glpat-ExaMplEKeyExAmplEKey"
+          }
+        ]
+      }
+    },
+    "Clusters": {
+      "NuGetCluster": {
+        "Destinations": {
+          "Nuget": {
+            "Address": "https://my.gitlab.org/api/v4/projects/69/packages/nuget"
+          }
+        }
+      }
+    }
+  },
+  "Logging": {
+    "LogLevel": {
+      "Default": "Information",
+      "Microsoft": "Warning",
+      "Yarp": "Warning",
+      "Microsoft.Hosting.Lifetime": "Information"
+    }
+  }
+}
+```
+
+The values you need to set / change are the following:
+
+* `Url`: The URL(s)/port(s) at which the proxy will be listening.
+* `PathBase`: (OPTIONAL) The path base that the proxy will be using. This is useful if you want to host multiple proxies on the same server.
+* `ApiKey`: The API key that will be used to authenticate with the NuGet server.
+* `Address`: The URL of the NuGet server NuGet feed.
+
+Next, start the proxy and configure your NuGet client to use the proxy: set the `source` to the URL of the proxy + `PathBase` and add `index.json` to the end of the URL. For example: if you configured the `Urls` as `http://localhost:5000/` and `PathBase` as `MyNuGet` then set the NuGet source for your client to `http://localhost:5000/MyNuGet/index.json`.
+
+## What accesstoken?
+
+For GitLab, you can create an access token by going to the NuGet repository -> Settings -> Access Tokens -> Add new Token. Give the token a name (like `NuGetProxy`), expiration data and give `read_api` permissions.
+
+Note that when the token expires, you will need to update the token in the appsettings and restart the proxy.
+
+## Can I push via the proxy?
+
+No, the proxy is read-only. If you want to push packages, you will need to push them directly to the NuGet server.
+
+## HTTPS & TLS
+
+You can use HTTPS & TLS; documentation on how to configure YARP can be found [here](https://microsoft.github.io/reverse-proxy/articles/https-tls.html).
